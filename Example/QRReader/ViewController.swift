b69418120692011c275fb2aae5de2bde411d261a
KiraSoga <sogakira0202@gmail.com>
2022-08-18 16:11:19 +0900

    QRコード読み取り内容全般

--- Example/QRReader/ViewController.swift
+++ Example/QRReader/ViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 import QRReader
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    private let myQRCodeReader = QRReader()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        var a = MyClass()
-        print(a.a)
+        self.myQRCodeReader.delegate = self
+        self.myQRCodeReader.setupCamera(view:self.view)
+        //読み込めるカメラ範囲
+        self.myQRCodeReader.readRange()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
+extension ViewController: AVCaptureMetadataOutputObjectsDelegate{
+    //対象を認識、読み込んだ時に呼ばれる
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+        //一画面上に複数のQRがある場合、複数読み込むが今回は便宜的に先頭のオブジェクトを処理
+        if let metadata = metadataObjects.first as? AVMetadataMachineReadableCodeObject{
+            let barCode = self.myQRCodeReader.previewLayer.transformedMetadataObject(for: metadata) as! AVMetadataMachineReadableCodeObject
+            //読み込んだQRを映像上で枠を囲む。ユーザへの通知。必要な時は記述しなくてよい。
+            self.myQRCodeReader.qrView.frame = barCode.bounds
+            //QRデータを表示
+            if let str = metadata.stringValue {
+                print(str)
+                let alert = UIAlertController(title: "スキャン完了",
+                                              message: str,
+                                              preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(okAction)
+                self.present(alert, animated: true)
+            }
+        }
+    }
+}
--- QRReader/Classes/QrReader.swift
+++ QRReader/Classes/QrReader.swift
@@ -1,13 +1,90 @@
-import Foundation
+import UIKit
+import AVFoundation
 
-public extension String {
-    func shortString() -> String {
-        return "\(self.prefix(10))..."
-    }
-}
-
-public class MyClass {
+public class QRReader {
     public init() {}
-
-    public var a = 2
+    
+    public let captureSession = AVCaptureSession()
+    public let videoDevice = AVCaptureDevice.default(for: .video)
+    public var metadataOutput = AVCaptureMetadataOutput()
+    
+    public var forwardDelegate: AVCaptureMetadataOutputObjectsDelegate?
+    public var preview: UIView?
+    public var previewLayer = AVCaptureVideoPreviewLayer()
+    public let qrView = UIView()
+    
+    public var delegate: AVCaptureMetadataOutputObjectsDelegate? {
+        get { self.forwardDelegate }
+        set(delegate) {
+            self.forwardDelegate = delegate
+            self.metadataOutput.setMetadataObjectsDelegate(delegate, queue: .main)
+        }
+    }
+    
+    public func setupCamera(view: UIView, borderWidth: Int = 2, borderColor: UIColor = .green) {
+        
+        self.preview = view
+        
+        do {
+            let videoInput = try AVCaptureDeviceInput(device: self.videoDevice!) as AVCaptureDeviceInput
+            self.captureSession.addInput(videoInput)
+        } catch {
+            print(error)
+        }
+        
+        self.captureSession.addOutput(self.metadataOutput)
+        
+        self.metadataOutput.metadataObjectTypes = [.qr]
+        
+        self.cameraPreview(view)
+        
+        self.targetCapture(borderWidth:borderWidth, borderColor: borderColor)
+        
+        self.captureSession.startRunning()
+    }
+    
+    // 画面に表示
+    private func cameraPreview(_ view: UIView) {
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        self.previewLayer.frame = view.bounds
+        self.previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(self.previewLayer)
+    }
+    
+    private func targetCapture(borderWidth: Int, borderColor: UIColor) {
+        self.qrView.layer.borderWidth = CGFloat(borderWidth)
+        self.qrView.layer.borderColor = borderColor.cgColor
+        self.qrView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        if let v = self.preview {
+            v.addSubview(qrView)
+        }
+    }
+    
+    //読み取り範囲の指定
+    public func readRange(frame: CGRect = CGRect(x: 0.2,
+                                                 y: 0.3,
+                                                 width: 0.6,
+                                                 height: 0.4)) {
+        
+        self.metadataOutput.rectOfInterest = CGRect(x: frame.minY,
+                                                    y: 1 - frame.minX - frame.size.width,
+                                                    width: frame.size.height,
+                                                    height: frame.size.width)
+        
+        let view = UIView()
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.red.cgColor
+        if let preview = self.preview {
+            view.frame = CGRect(x: preview.frame.size.width * frame.minX,
+                                y:  preview.frame.size.height * frame.minY,
+                                width: preview.frame.size.width * frame.size.width,
+                                height: preview.frame.size.height * frame.size.height)
+            preview.addSubview(view)
+        }
+    }
+    
+    // オブジェクトを読み込んだ時のdelegate AVCaptureMetadataOutputObjectsDelegate.metadataOutput
+    func delegate(_ delegate: AVCaptureMetadataOutputObjectsDelegate) {
+        self.metadataOutput.setMetadataObjectsDelegate(delegate, queue: .main)
+    }
 }
