b92ae6380d65284f888224dea8227e76e4bb8457
KiraSoga <sogakira0202@gmail.com>
2022-08-18 14:24:03 +0900

    no message

---
+++ Example/Pods/Target Support Files/Pods-QRReader_Example/Pods-QRReader_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QRReader_Example : NSObject
+@end
+@implementation PodsDummy_Pods_QRReader_Example
+@end
---
+++ Example/Pods/Target Support Files/QRReader/QRReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QRReader : NSObject
+@end
+@implementation PodsDummy_QRReader
+@end
